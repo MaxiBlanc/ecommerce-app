c7e5be7541b8cffa6427c4f5c6f4cb972be797f7
MaxiBlanc <maxiblanc240801@gmail.com>
2025-07-14 01:44:56 -0300

    act10

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { getDocs, collection } from 'firebase/firestore';
+import { db } from '../firebase/firebaseconfig';
+
 
 export default function ProductList() {
   const [products, setProducts] = useState([]);
